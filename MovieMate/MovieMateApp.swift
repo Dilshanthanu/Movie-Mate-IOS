ffdf0cee84e38d9a9f6a597fcc66cab14924025a
Dilshanthanu <dilshanthanushka98@gmail.com>
2025-11-19 00:23:29 +0530

    Initial Commit

---
+++ MovieMate/MovieMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMateApp.swift
+//  MovieMate
+//
+//  Created by Dilshan Thanushka on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct MovieMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
